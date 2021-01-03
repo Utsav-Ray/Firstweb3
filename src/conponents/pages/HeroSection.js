08b7dfd9ada73cd3afdebfce38902487b784bf99
Utsav Ray <utsavray09@gmail.com>
2021-01-03 21:21:41 +0530

    my first web

---
+++ src/conponents/pages/HeroSection.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './HeroSection.css';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+
+function HeroSection({
+  lightBg,
+  topLine,
+  lightText,
+  lightTextDesc,
+  headline,
+  description,
+  buttonLabel,
+  img,
+  alt,
+  imgStart
+}) {
+  return (
+    <>
+      <div
+        className={lightBg ? 'home__hero-section' : 'home__hero-section darkBg'} >
+       
+
+
+
+        
+      </div>
+    </>
+  );
+}
+
+export default HeroSection;
